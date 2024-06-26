378c6c5d1270cf83364a3b2101597613856b6b31
Boishakhi Ghosh Mukta <141757309+Boishakhi11@users.noreply.github.com>
2024-06-26 23:43:11 +0200

    break statement

---
+++ break.cs
@@ -0,0 +1,98 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) 
+        {
+          break;
+        }
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
+
+//output is :
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) 
+        {
+          break;
+        }
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
+using System;
+​
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) 
+        {
+          break;
+        }
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
+/* output is 
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) 
+        {
+          break;
+        }
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
+using System;
+​
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      for (int i = 0; i < 10; i++) 
+      {
+        if (i == 4) 
+        {
+          break;
+        }
+        Console.WriteLine(i);
+      }    
+    }
+  }
+}
