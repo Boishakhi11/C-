62879064aaef96be6ea3760addd83e8af5a628c7
Boishakhi Ghosh Mukta <141757309+Boishakhi11@users.noreply.github.com>
2024-06-27 00:15:02 +0200

    return values of method

---
+++ method2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static int MyMethod(int x)
+    {
+      return 5 + x;
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine(MyMethod(3));
+    }
+  }
+}
